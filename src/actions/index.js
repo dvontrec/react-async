2b709d782302908eb870258ac54264653aced03f
Dvontre Coleman <dvontrec@gmail.com>
2019-01-17 15:55:46 -0500

    Add lodash to reduce requests for users

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,14 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  // fetches posts
+  await dispatch(fetchPosts());
+  // returns an array of unique user ids
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // fetches the users for each user id
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
 //  Creates an async action creator
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
@@ -18,3 +27,17 @@ export const fetchUser = id => async dispatch => {
     payload: response.data
   });
 };
+
+// Creates a memoised version of fetch user to reduce api calls
+// action to fetch one user as a time
+// export const fetchUser = id => dispatch => {
+//   _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({
+//     type: 'FETCH_USER',
+//     payload: response.data
+//   });
+// });
