c40675ee1ef447fc471cbadd7afb4c088cd94b35
Dvontre Coleman <dvontrec@gmail.com>
2019-01-17 13:38:26 -0500

    Fix post rendering

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 //  Creates an async action creator
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
-  // dispatches this object when functino has been ran
+  // dispatches this object when function has been ran
   dispatch({
     type: 'FETCH_POSTS',
     payload: response.data
